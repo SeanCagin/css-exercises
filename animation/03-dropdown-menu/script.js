9b58b5db208c65386bbaad0a3976ba195279157c
Sean Uz <cuz3@gatech.edu>
2024-09-23 01:03:44 -0400

    Finish project 3 with a nice finish
    ps: turned their nice js code into spaghetti.

--- animation/03-dropdown-menu/script.js
+++ animation/03-dropdown-menu/script.js
@@ -2,14 +2,40 @@ const dropdownContainer = document.querySelector(".dropdown-container");
 const menuTitle = document.querySelector(".menu-title");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
+let state = 1;
+
 menuTitle.addEventListener("click", (e) => {
+  if (state == 0) return;
   if (e.target === e.currentTarget) {
-    dropdownMenu.classList.toggle("visible");
-  }  
-})
+    state = 0;
+    if (dropdownMenu.classList.contains("visible")) {
+      dropdownMenu.classList.toggle("visible");
+      dropdownMenu.classList.add("int");
+      setTimeout(() => {
+        dropdownMenu.classList.remove("int");
+        state = 1;
+      }, 1000);
+    } else {
+      dropdownMenu.classList.toggle("visible");
+      setTimeout(() => {
+        state = 1;
+      }, 1000);
+    }
+  }
+});
 
 window.addEventListener("click", (e) => {
-  if (!dropdownContainer.contains(e.target)) {
-    dropdownMenu.classList.remove("visible")
+  if (state == 0) return;
+  if (
+    !dropdownContainer.contains(e.target) &&
+    dropdownMenu.classList.contains("visible")
+  ) {
+    state = 0;
+    dropdownMenu.classList.remove("visible");
+    dropdownMenu.classList.add("int");
+    setTimeout(() => {
+      dropdownMenu.classList.remove("int");
+      state = 1;
+    }, 1000);
   }
-})
+});
